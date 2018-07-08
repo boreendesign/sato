1fcbe46f01323cf15c6c87b24c4e77e7a4113952
Stephen Morrissey <info@boreendesign.com>
2018-07-08 13:01:23 -0700

    Initial Push

--- src/sato-reviews.js
+++ src/sato-reviews.js
@@ -7,3 +7,19 @@ function simpleTemplating(data) {
     html += '</div>';
     return html;
 }
+
+function setPagination(ref){
+  ref.on("value", function(snapshot) {
+    $('#pagination-container').pagination({
+        dataSource: snapshot.val().posts,
+        pageSize:3,
+        callback: function(data, pagination) {
+            var html = simpleTemplating(data);
+            $('#data-container').html(html);
+        }
+    })
+     console.log(snapshot.val());
+  }, function (error) {
+     console.log("Error: " + error.code);
+  });
+}
