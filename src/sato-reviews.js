a6e2b3f94dfca989bee9c809655825efe481c42a
Stephen Morrissey <info@boreendesign.com>
2018-07-08 17:28:28 -0700

    Initial

--- src/sato-reviews.js
+++ src/sato-reviews.js
@@ -4,7 +4,7 @@ function simpleTemplating(data) {
     $.each(data, function(index, item){
         html += '<div class="review"><div class = "review_name">'+ item.name +'</div>';
         html += '<div class = "review_desc">'+ item.review +'</div>';
-        html += '<div class = "review_rating'+ review_counter + '"></div></div>';
+        html += '<div class="rating" data-rate-value=' + item.rating + '></div></div>';
         review_counter++;
     });
     html += '</div>';
@@ -19,7 +19,18 @@ function setPagination(ref,numberPerPage,totalNumber){
         pageSize:numberPerPage,
         callback: function(data, pagination) {
             var review_counter = simpleTemplating(data);
+            var options = {
+                max_value: 5,
+                step_size: 0.5,
+                initial_value: 0,
+                selected_symbol_type: 'utf8_star', // Must be a key from symbols
+                cursor: 'default',
+                readonly: false,
+                change_once: false, // Determines if the rating can only be set once
+                additional_data: {} // Additional data to send to the server
+            }
 
+            $(".rating").rate(options);
         }
     })
      console.log(snapshot.val());
